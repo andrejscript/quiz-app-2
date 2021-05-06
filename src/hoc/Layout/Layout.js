5bc36254002e02f46f55706086de84c63a4352e2
AndreWeb <andzion105@gmail.com>
2021-05-06 13:29:46 +0300

    clear project

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
-// import classes from './Layout.module.css';
+import classes from './Layout.module.css';
    
 export default class Layout extends Component {
    
 render() {
+  
   return (  
-    <div>
+    <div className={classes.Layout}>
       <main>{this.props.children}</main>
+
+
+
+      {
+        <a href="javascript:void(document.body.style.backgroundColor='green');">
+  Click here for green background
+</a>
+
+      }
     </div>
+    
     );
   }
 }
