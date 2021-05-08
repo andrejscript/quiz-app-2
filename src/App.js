c8ba013a1b34951b7b5e591909b40c3ccfd81624
AndreWeb <andzion105@gmail.com>
2021-05-08 14:35:23 +0300

    add styles to main page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import Layout from './hoc/Layout/Layout';
+import Quiz from './containers/Quiz/Quiz';
 
 function App() {
   return (
     <Layout>
-      <div style={{width: '400px', border: '1px solid #000'}}>
-        <h1>Layout</h1>
-      </div>
+        <Quiz />
     </Layout>
   );
 }
