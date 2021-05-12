1046cd002433542140ab8d410d63493f78b25a24
AndreWeb <andzion105@gmail.com>
2021-05-12 21:57:17 +0300

    temporary deleting components

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import classes from './Quiz.module.css';
-import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz';
+// import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz';
 
 export default class Quiz extends Component {
   state = {
     quiz: [
       {
+        question: 'What is your profession?',
         answers: [
           {text: 'Question #1', id: 1 },
           {text: 'Question #2', id: 2 },
@@ -21,7 +22,10 @@ export default class Quiz extends Component {
       <div className={classes.Quiz}>
         <div className={classes.QuizWrapper}>
           <h1>Answer all questions</h1>
-          <ActiveQuiz answers={this.state.quiz[0].answers}/>
+          <ActiveQuiz 
+            question={this.state.quiz[0].question}
+            answers={this.state.quiz[0].answers}
+            />
         </div>
       </div>
     );
