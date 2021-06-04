90e1e8c2a96f1892e954904d9466033da10fa3cb
AndreWeb <andzion105@gmail.com>
2021-06-05 01:59:03 +0300

    add Drawer with links

---
+++ src/components/Navigation/Drawer/Drawer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import classes from './Drawer.module.css';
+
+export default class Drawer extends Component {
+  
+  
+  render() {
+    return <nav></nav>;
+  }
+}
