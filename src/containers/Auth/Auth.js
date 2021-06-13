04bc5f996408c981a9f0a6f668e18d05c314917e
andrejscript <andzion105@gmail.com>
2021-06-13 21:19:59 +0300

    add Auth styles

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,20 @@ import classes from './Auth.module.css';
 import Button from '../../components/UI/Button/Button'
 
 export default class Auth extends Component {
+
+  submitHandler = (e) => {
+    e.preventDefault();
+  }
+
+  loginHandler = (e) => {
+    console.log(`1:`, e);
+  }
+  
+  registerHandler = (e) => {
+    console.log(`2:`, e);
+    
+  }
+
   render() {
     return (
       <div className={classes.Auth}>
@@ -10,12 +24,12 @@ export default class Auth extends Component {
 
           <h1>Auth</h1>
 
-          <form>
+          <form onSubmit={this.submitHandler}>
             <input type='text' />
-              <input type='text' />
-                <Button type='success' onClick={this.loginHandler}>Sign In</Button>
-                <Button type='success' onClick={this.registerHandler}>Sign Up</Button>
-        </form>
+            <input type='text' />
+            <Button type='success' onClick={this.loginHandler}>Sign In</Button>
+            <Button type='primary' onClick={this.registerHandler}>Sign Up</Button>
+          </form>
         </div>
       </div>
     );
