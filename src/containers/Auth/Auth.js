c8dfcd04291e670aa135658dbac32db2b17208d2
andrejscript <andzion105@gmail.com>
2021-06-13 14:46:25 +0300

    add and markup Auth Component

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-// import classes from './Auth.module.css';
+import classes from './Auth.module.css';
+import Button from '../../components/UI/Button/Button'
 
 export default class Auth extends Component {
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={classes.Auth}>
+        <div>
+
+          <h1>Auth</h1>
+
+          <form>
+            <input type='text' />
+              <input type='text' />
+                <Button type='success' onClick={this.loginHandler}>Sign In</Button>
+                <Button type='success' onClick={this.registerHandler}>Sign Up</Button>
+        </form>
+        </div>
       </div>
     );
   }
