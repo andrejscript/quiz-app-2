63a9b7d7bc97516d4b6f5fd00afe5b7cac8d6c84
AndreWeb <andzion105@gmail.com>
2021-06-22 20:27:06 +0300

    add Validetion form

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import classes from './Input.module.css';
+
+function isInvalid({ valid, touched, shouldValidate }) {}
+
 const Input = props => {
   const inputType = props.type || 'text',
     cls = [classes.Input],
     htmlFor = `${inputType}-${Math.random()}`;
 
+  if (isInvalid(props)) {
+    cls.push(classes.invalid);
+  }
+
   return (
     <div className={cls.join(' ')}>
-      <label htmlFor=''>
-        <input
-          type={inputType}
-          id={htmlFor}
-          value={props.value}
-          onChange={props.onChange}
-        />
-      </label>
+      <label htmlFor={inputType}>{props.label}</label>
+      <input
+        type={inputType}
+        id={htmlFor}
+        value={props.value}
+        onChange={props.onChange}
+      />
+
+      {isInvalid(props) ? (
+        <span>{props.errorMessage || 'Enter correct values'}</span>
+      ) : null}
     </div>
   );
 };
--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,34 +1,36 @@
 import React, { Component } from 'react';
 import classes from './Auth.module.css';
-import Button from '../../components/UI/Button/Button'
+import Button from '../../components/UI/Button/Button';
+import Input from '../../components/UI/Input/Input';
 
 export default class Auth extends Component {
-
-  submitHandler = (e) => {
+  submitHandler = e => {
     e.preventDefault();
-  }
+  };
 
-  loginHandler = (e) => {
+  loginHandler = e => {
     console.log(`1:`, e);
-  }
-  
-  registerHandler = (e) => {
+  };
+
+  registerHandler = e => {
     console.log(`2:`, e);
-    
-  }
+  };
 
   render() {
     return (
       <div className={classes.Auth}>
         <div>
-
           <h1>Auth</h1>
 
           <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-            <input type='text' />
-            <input type='text' />
-            <Button type='success' onClick={this.loginHandler}>Sign In</Button>
-            <Button type='primary' onClick={this.registerHandler}>Sign Up</Button>
+            <Input label='Email' />
+            <Input label='Password' />
+            <Button type='success' onClick={this.loginHandler}>
+              Sign In
+            </Button>
+            <Button type='primary' onClick={this.registerHandler}>
+              Sign Up
+            </Button>
           </form>
         </div>
       </div>
